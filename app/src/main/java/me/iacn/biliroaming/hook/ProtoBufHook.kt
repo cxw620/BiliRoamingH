d7a0f1a2fd112ceeec5ec489fc6431aa2b6e3ff0
陈寒彤 <cxwdyx620@gmail.com>
2023-08-19 20:15:18 +0800

    feat: replace view page charge btn

--- app/src/main/java/me/iacn/biliroaming/hook/ProtoBufHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/ProtoBufHook.kt
@@ -66,6 +66,7 @@ class ProtoBufHook(classLoader: ClassLoader) : BaseHook(classLoader) {
         val blockCommentGuide = sPrefs.getBoolean("block_comment_guide", false)
         val blockVideoComment = sPrefs.getBoolean("block_video_comment", false)
         val blockViewPageAds = sPrefs.getBoolean("block_view_page_ads", false)
+        val removeViewPageChargeBtn = sPrefs.getBoolean("remove_view_page_charge_btn", false)
 
         val blockFollowButton = sPrefs.getStringSet("block_follow_button", null).orEmpty()
         if (blockFollowButton.isNotEmpty()) {
@@ -131,6 +132,9 @@ class ProtoBufHook(classLoader: ClassLoader) : BaseHook(classLoader) {
             if (hidden && removeUpVipLabel) {
                 param.result.callMethod("getOwnerExt")?.callMethod("getVip")
                     ?.callMethod("clearLabel")
+            }
+            if (removeViewPageChargeBtn) {
+                param.result.callMethod("getReqUser")?.callMethod("clearElecPlusBtn")
             }
         }
 
@@ -390,6 +394,10 @@ class ProtoBufHook(classLoader: ClassLoader) : BaseHook(classLoader) {
 
             if (blockViewPageAds) {
                 param.result.callMethod("clearCm")
+            }
+
+            if (removeViewPageChargeBtn) {
+                param.result.callMethod("getReqUser")?.callMethod("clearElecPlusBtn")
             }
 
             param.result.callMethod("getTab")?.run {
